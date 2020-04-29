53a7beac96a2239e9699f7d9013af6874ae7ce52
JR-Morgan <45512892+JR-Morgan@users.noreply.github.com>
2020-04-29 21:07:11 +0100

    Added JS to items/create.blade.php to allow for user friendly addition of images

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,7 +1,40 @@
 <!--inherite master template app.blade.php -->
 @extends('layouts.app')
+
 <!--define the content section -->
 @section('content')
+
+<!--image input script -->
+<script>
+var counter = -1;
+
+document.body.onload = function() {
+        addImageInput()
+    };
+
+function addImageInput()
+{
+    counter++
+    var id = `image${counter.toString()}`
+
+    var newInput = document.createElement("input");
+    newInput.setAttribute("id", id);
+    newInput.setAttribute("type", "file");
+    newInput.setAttribute("name", id);
+    newInput.setAttribute("placeholder", "Image file");
+    newInput.setAttribute("onChange", "checkAddImageInputEnabled()");
+
+    document.getElementById('image-upload').appendChild(newInput);
+
+    checkAddImageInputEnabled();
+}
+
+function checkAddImageInputEnabled()
+{
+    document.getElementById('add-input-button').disabled = document.getElementById(`image${counter.toString()}`).files.length == 0;
+}
+</script>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10 ">
@@ -64,9 +97,13 @@
                             </div>
                             <div class="row col-md-10">
                                 <label class="col-md-5">Image</label>
-                                <input class="col-md-7" type="file" name="image0" placeholder="Image file" />
-                                <input class="col-md-7" type="file" name="image1" placeholder="Image file" />
-                                <input class="col-md-7" type="file" name="image2" placeholder="Image file" />
+                                <div id="image-upload" class="col-md-7"></div>
+                                <div class="col-md-12">
+                                    <input id="add-input-button" style="height:2em;width:2em;" type="button" value="+" onclick="addImageInput()">
+                                </div>
+                            </div>
+                            <div class="row col-md-12">
+
                             </div>
                         </div>
                         <div class="col-md-7 col-md-offset-4">
